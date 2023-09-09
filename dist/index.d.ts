ed3492f281f57f9524b4c21a3fbdb2f59814e466
Robin Löffel <hi@robinloeffel.ch>
2023-09-09 22:14:04 +0200

    chore: update .gitignore

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { Transform } from "node:stream";
-import { type Options } from "sass";
-declare const _default: (options: Options<"sync">) => Transform;
-export = _default;
